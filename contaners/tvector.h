03c7c1d69ac241244eb2e3349db1f6da13f90ed0
s179057 <s179057@SEA-8D0A81>
2017-11-29 12:02:11 -0800

    yo

--- contaners/tvector.h
+++ contaners/tvector.h
@@ -1,12 +1,33 @@
 #pragma once
 #include <cassert>
 #include <cstring>
+#include <iostream>
 template<typename t>
 class tVector 
 {
 	t*data;
 	size_t capacity;
 	size_t size;
+	bool grow(size_t minSize)
+	{
+		assert(minSize <= 64000);
+		if (minSize <= capacity)
+		{
+			return true;
+		}
+
+		while (capacity<minSize)
+		{
+			capacity *= 2;
+		}
+		t* newData = new t[capacity];
+		memcpy(newData, data, sizeof(t) * size);
+
+		delete[]data;
+
+		data = newData;
+		return true;
+	}
 public:
 	tVector() 
 	{
@@ -53,21 +74,94 @@ public:
 	}
 	t front() const 
 	{
+		assert(size > 0);
 		return data[0];
 	}
 	t back() const 
 	{
+		assert(size > 0);
 		return data[size - 1];
+	}
+	void pop() 
+	{
+		assert(size > 0);
+		size -= 1;
 	}
 	void erases(size_t idx) 
 	{
-		for (int i = idx; i < size; i++)
+		for (size_t i = idx; i < size; i++)
 		{
 			t temp = data[i];
 			data[i] = data[i + 1];
 			data[i + 1] = temp;
 		}
 		size--;
+	}
+	void clear() 
+	{
+
+		size = 0;
+	}
+	t operator[](size_t idx) const 
+	{
+		assert(idx < size);
+		return data[idx];
+	}
+	t &operator[](size_t idx) 
+	{
+		assert(idx < size);
+		return data[idx];
+	}
+	size_t count(t value) const
+	{
+		size_t counter = 0;
+		for (size_t i = 0; i < size; i++)
+		{
+			if (data[i] == value)
+			{
+				counter++;
+			}
+		}
+		return counter;
+	}
+	void insert(size_t idx, t value) 
+	{
+		assert(idx >= 0);
+		assert(idx < size);
+		append(value);
+		if (idx<size - 1)
+		{
+			for (size_t i = size; i >= idx; i--)
+			{
+				t temp = data[i];
+				data[i] = data[i + 1];
+				data[i + 1] = temp;
+			}
+		}
+	}
+	void Reserve(size_t newCapacity) 
+	{
+
+		if (newCapacity > capacity)
+		{
+			t *newData = new t[newCapacity];
+			memcpy(newData, data, sizeof(t) * size);
+			delete[] data;
+			data = newData;
+			capacity = newCapacity;
+		}
+	}
+	void Compact() 
+	{
+
+		if (capacity > size)
+		{
+			t *newData = new t[size];
+			memcpy(newData, data, sizeof(t) * size);
+			delete[] data;
+			data = newData;
+			capacity = size;
+		}
 	}
 
 };
