9b709e0e80085c34c3cef0a867bfa6fded15d926
s179057 <s179057@SEA-8CC916>
2017-12-18 09:26:08 -0800

    yo

--- contaners/deque.h
+++ contaners/deque.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "tlinkedlist.h"
+#include "iterator.h"
 template <typename T>
 class Deque
 {
@@ -38,11 +39,52 @@ public:
 	}
 	int at(T index) 
 	{
+		return mylinckedlist.at(index);
+	}
+	void assign(size_t idx, const T vel)
+	{
+		for (int i = 0; i < size; i++) 
+		{
+			idx;
+		}
+	}
+	void erese() 
+	{
+		return mylinckedlist.erase();
+	}
+	void clear() 
+	{
+		return mylinckedlist.clear();
+	}
+	bool empty() 
+	{
+		return mylinckedlist.empty();
+	}
+	size_t size() const 
+	{
+		return mylinckedlist.size();
+	}
+
+	Deque() 
+	{
+		return mylinckedlist.tLnkedlist();
+	}
+	~Deque()
+	{
+		return mylinckedlist.~tLnkedlist();
+	}
+	void swap(T& x) 
+	{
 
 	}
-		
+	size_t max_size() const  noexcept
+	{
+		return mylinckedlist.size();
+	}
+	void resize(size_t idx,const T& val) 
+	{
 
-
+	}
 private:
 	tlinkedlist<T> mylinckedlist;
 };
